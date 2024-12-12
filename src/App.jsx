1df981da33df9445fc74ee4bea3caa3ebc7ff2bf
Pedro Oliveira <pedrogagodev@gmail.com>
2024-12-12 01:32:10 -0300

    Adds the Modal component and the buttons for open and close it

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,25 @@
 import { Header } from "./components/Header"
 import { Footer } from "./components/Footer"
+import { Modal } from "./components/Modal"
+import { useState } from "react"
 
 function App() {
 
+  const [openModal, setOpenModal] = useState(false)
+
+  const handleModalOpen = () => {
+    setOpenModal(true)
+  }
+
+  const handleModalClose = () => {
+    setOpenModal(false)
+  }
 
   return (
     <>
+      <Modal isOpen={openModal} onModalClose={handleModalClose}/>
       <Header />
-      <Footer />
+      <Footer onModalOpen={handleModalOpen}/>
 
     </>
   )
