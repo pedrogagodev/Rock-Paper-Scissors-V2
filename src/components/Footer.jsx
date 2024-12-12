81f434ae4b98b7c97f8b68be92b4ccd6ae6934be
Pedro Oliveira <pedrogagodev@gmail.com>
2024-12-12 01:23:24 -0300

    Add the Footer component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+export function Footer() {
+    return (
+        <footer className="flex flex-col h-24 absolute bottom-0 w-full bg-blue-950">
+            <div className="flex flex-col h-full justify-between items-center">
+                <button className="bg-orange-300 mt-4 p-2 rounded-lg text" >View Rules</button>
+                <small className="text-white text-sm">Developed by <a href="">Pedro Oliveira</a></small>
+            </div>
+
+
+        </footer>
+    )
+}
